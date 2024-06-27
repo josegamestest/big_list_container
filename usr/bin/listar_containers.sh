7099177421f4b09a3c887a203009718a8fe2af7e
Jose Anastacio <joseanastaciocastilho@gmail.com>
2024-06-27 12:55:25 -0300

    Update code
    Add, restart page, start, stop and list docker service,
    modified CSS style file from /usr/share/ to /usr/share/big_list_container/

--- usr/bin/listar_containers.sh
+++ usr/bin/listar_containers.sh
@@ -7,39 +7,57 @@ OUTPUT_PS=$(docker ps)
 OUTPUT_PS_A_Q=$(docker ps -a -q)
 OUTPUT_IMAGES=$(docker images )
 
+OUTPUT_STATUS=$(systemctl status docker.service)
 
 echo "
 <!DOCTYPE html>
 <html lang='en'>
-<head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    
-    <title>Docker Status</title>
-    <link rel='stylesheet' href='usr/share/big_list_container/styles.css'>
-</head>
-<body>
-
-    <div class='titulo' id='titulo'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        
+        <title>Docker Status</title>
+        
+        <link rel='stylesheet' href='usr/share/big_list_container/styles.css'>
+        
+        <script>
+            function paginarestart(){
+                window.location.reload();
+            } 
+        </script>
+    </head>
+    <body>
+        
+        <div class='titulo' id='titulo'>  
             <h1>Listagem Docker Servidor</h1>
-    
-    <div class='content' id='docker-ps'>
-            <h1>Listagem Docker Servidores ativos</h1>
-            <pre>$OUTPUT_PS  </pre>
-    </div>
-    
-    <div id='container'>
+            
+            
+            <a onclick=\"_run('systemctl start docker.service')\" class='botao botao1'>Start DOCKER</a>
+            <a onclick=\"_run('systemctl stop docker.service')\" class='botao botao2'>Stop DOCKER</a>
+            <a onClick=\"paginarestart()\" class='botao botao1'>Recarega_pag</a>
+            
+        </div>
         
         <div class='content' id='docker-ps'>
-            <h2>Containers po ID</h2>
-            <pre>$OUTPUT_PS_A_Q</pre>
+                <h1>Listagem Docker Servidores ativos</h1>
+                <pre>$OUTPUT_PS  </pre>
         </div>
-        <div class='content' id='docker-images'>
-            <h2>Docker Images</h2>
-            <pre>$OUTPUT_IMAGES</pre>
+        
+        <div id='container'>
+            <div class='content' id='docker-ps'>
+                <h2>Containers po ID</h2>
+                <pre>$OUTPUT_PS_A_Q</pre>
+            </div>
+            <div class='content' id='docker-images'>
+                <h2>Docker Images</h2>
+                <pre>$OUTPUT_IMAGES</pre>
+            </div>
         </div>
-    </div>
-    </div>
-</body>
+        <div id='container'>
+            <div class='status' id='status'>  
+                    <pre>$OUTPUT_STATUS</pre>
+            </div>
+        </div>
+    </body>
 </html>
 "
